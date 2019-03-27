c1a2febdeb30f4c75013ff5d9f04df6b78eb7235
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-03-27 00:52:58 -0300

    feat(bitmap): convert code to a starter parser

--- bitmap-decoder/decoder.c
+++ bitmap-decoder/decoder.c
@@ -5,96 +5,51 @@
 typedef struct {
 	unsigned int size;
 	unsigned int headerSize;
+	unsigned int width;
+	unsigned int height;
 } BitmapImage;
 
-void printGroupedColors();
+void printFooter(BitmapImage *image);
+void parse(BitmapImage *image, FILE *file);
 
 int main(void)
 {
 	FILE *bitmapFile;
 	BitmapImage bitmapImage;
-	unsigned short int content[1024];
-	unsigned short int columnCursor = 0;
-	unsigned short int colorsCursor = 0;
-	unsigned int currentByte = 0;
-	unsigned int readBytes = 0;
 
 	bitmapFile = fopen("/home/stanley/Desktop/x.bmp", "rb");
+	parse(&bitmapImage, bitmapFile);
+	fclose(bitmapFile);
 
-	for ((currentByte = fgetc(bitmapFile)); currentByte != EOF; (currentByte = fgetc(bitmapFile)), readBytes++) {
-		content[readBytes] = currentByte;
+	printFooter(&bitmapImage);
+	return 0;
+}
 
-		// header has started
-		if (readBytes <= 54) {
-			if (readBytes == 0) {
-				puts("Header: ");
-				columnCursor = 0;
-			}
+void printFooter(BitmapImage *image)
+{
+	putchar('\n');
+	puts("--------------------------------------");
+	printf("Header in bytes: ..................... %uB \n", image->headerSize);
+	printf("Total in bytes: ...................... %uB \n", image->size);
+	putchar('\n');
+}
 
-			if (columnCursor == 0) {
-				putchar('[');
-			}
+void parse(BitmapImage *image, FILE *file)
+{
+	int currentByte;
+	unsigned short int cursor = 0;
 
-			printf("%02X", currentByte);
-
-			columnCursor++;
-
-			if (columnCursor == 3 || readBytes == 54) {
-				colorsCursor++;
-				columnCursor = 0;
-				printf("] ");
-			}
-
-			if (readBytes == 54 || colorsCursor == COLORS_PER_LINE) {
-				colorsCursor = 0;
-				putchar('\n');
-			}
-		}
+	while ((currentByte = fgetc(file)) != EOF) {
+		cursor++;
 
 		// bfSize
-		if (readBytes == 2) {
-			bitmapImage.size = currentByte;
+		if (cursor == 3) {
+			image->size = currentByte;
 		}
 
 		// bfOffBits
-		else if (readBytes == 10) {
-			bitmapImage.headerSize = currentByte;
-		}
-
-		// content has started
-		else if (readBytes >= bitmapImage.headerSize) {
-			if (readBytes == bitmapImage.headerSize) {
-				puts("Content:");
-			}
-
-			if (columnCursor == 0) {
-				putchar('[');
-			}
-
-			printf("%02X", currentByte);
-
-			columnCursor++;
-
-			if (columnCursor == 3) {
-				colorsCursor++;
-				columnCursor = 0;
-				printf("] ");
-			}
-
-			if (colorsCursor == COLORS_PER_LINE) {
-				colorsCursor = 0;
-				putchar('\n');
-			}
+		else if (cursor == 11) { // must read whole DWORD (4B)
+			image->headerSize = currentByte;
 		}
 	}
-
-	fclose(bitmapFile);
-
-	putchar('\n');
-	puts("------------------------");
-	printf("Header in bytes: %u \n", bitmapImage.headerSize);
-	printf("Total in bytes: %u \n", bitmapImage.size);
-	putchar('\n');
-
-	return 0;
 }
