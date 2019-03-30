951ba96c28fe1a2562c2a317e59c8e499eba33c4
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-03-30 03:57:09 -0300

    feat(bitmap): add "biWidth" and "biSize" props

--- bitmap-decoder/decoder.c
+++ bitmap-decoder/decoder.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 #define COLORS_PER_LINE 3
+#define WORD 2
+#define DWORD 4
+#define LONG 4
 
 typedef struct {
-	unsigned int size;
-	unsigned int headerSize;
-	unsigned int width;
-	unsigned int height;
+	unsigned int bfSize;
+	unsigned int bfOffBits;
+	unsigned int biWidth;
+	unsigned int biHeight;
 } BitmapImage;
 
 void printFooter(BitmapImage *image);
 void parse(BitmapImage *image, FILE *file);
+int readBytes(FILE *file, unsigned int bytes);
 
-int main(void)
+int main(int argc, char **argv)
 {
 	FILE *bitmapFile;
 	BitmapImage bitmapImage;
 
-	bitmapFile = fopen("/home/stanley/Desktop/x.bmp", "rb");
+	bitmapFile = fopen(argv[1], "rb");
 	parse(&bitmapImage, bitmapFile);
 	fclose(bitmapFile);
 
@@ -29,27 +33,51 @@ void printFooter(BitmapImage *image)
 {
 	putchar('\n');
 	puts("--------------------------------------");
-	printf("Header in bytes: ..................... %uB \n", image->headerSize);
-	printf("Total in bytes: ...................... %uB \n", image->size);
-	putchar('\n');
+	printf("Header size: ..................... %uB \n", image->bfOffBits);
+	printf("File size: ....................... %uB \n", image->bfSize);
+	printf("Width: ........................... %upx \n", image->biWidth);
+	printf("Height: .......................... %upx \n", image->biHeight);
+	puts("--------------------------------------");
 }
 
 void parse(BitmapImage *image, FILE *file)
 {
 	int currentByte;
-	unsigned short int cursor = 0;
+	long int cursor;
 
 	while ((currentByte = fgetc(file)) != EOF) {
-		cursor++;
+		cursor = ftell(file);
 
 		// bfSize
 		if (cursor == 3) {
-			image->size = currentByte;
+			image->bfSize = currentByte;
 		}
 
 		// bfOffBits
-		else if (cursor == 11) { // must read whole DWORD (4B)
-			image->headerSize = currentByte;
+		else if (cursor == 11) {
+			image->bfOffBits = currentByte + readBytes(file, (DWORD - 1));
+		}
+
+		// bfWidth
+		else if (cursor == 19) {
+			image->biWidth = currentByte + readBytes(file, (LONG - 1));
+		}
+
+		else if (cursor == 23) {
+			image->biHeight = currentByte + readBytes(file, (LONG - 1));
 		}
 	}
+}
+
+// int a[4] = {0x3a, 0x05, 0x00, 0x00}
+// int total = (a[0]) + (a[1] << 8) + (a[2] << 16) + (a[3] << 24)
+int readBytes(FILE *file, unsigned int bytes)
+{
+	int total = 0;
+
+	for (unsigned int i = 0; i < bytes; i++) {
+		total += (fgetc(file) << (8 * i));
+	}
+
+	return total;
 }
