77d64c407efe4b8c36e30459b37d4a26c8b6e09b
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-03-31 23:00:26 -0300

    feat(bitmap): add content parser

--- bitmap-parser/parser.c
+++ bitmap-parser/parser.c
@@ -7,6 +7,7 @@
 #define LONG 4
 #define ERROR_MISSING_FILENAME 15
 
+
 typedef struct {
 	unsigned int bfSize;
 	unsigned int bfOffBits;
@@ -15,12 +16,15 @@ typedef struct {
 	unsigned int biHeight;
 	unsigned int biBitCount;
 	unsigned int biSizeImage;
-	// int* content;
+	char* content;
 } BitmapImage;
 
+
+void printContent(BitmapImage *pImage);
 void printDetails(BitmapImage *pImage);
 void parse(BitmapImage *pImage, FILE *file);
 int readBytes(FILE *file, unsigned int bytes);
+
 
 int main(int argc, char **argv)
 {
@@ -40,10 +44,13 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+
 void printDetails(BitmapImage *pImage)
 {
 	putchar('\n');
 	puts("Content:");
+	printContent(pImage);
+	putchar('\n');
 	puts("--------------------------------------");
 	printf("File size: ....................... %uB \n", pImage->bfSize);
 	printf("Header length: ................... %uB \n", pImage->bfOffBits);
@@ -53,6 +60,18 @@ void printDetails(BitmapImage *pImage)
 	printf("Content length: .................. %uB \n", pImage->biSizeImage);
 	puts("--------------------------------------");
 }
+
+
+void printContent(BitmapImage *pImage)
+{
+	for (unsigned int i = 0; i < pImage->biSizeImage; i++) {
+		printf("%d ", pImage->content[i]);
+
+		if ((i % pImage->biWidth) == 0)
+			putchar('\n');
+	}
+}
+
 
 void parse(BitmapImage *pImage, FILE *file)
 {
@@ -84,20 +103,18 @@ void parse(BitmapImage *pImage, FILE *file)
 			pImage->biSizeImage = currentByte + readBytes(file, (DWORD - 1));
 
 		// content
-		/*
 		else if (cursor == (pImage->bfOffBits + 1)) {
-			printf("%d ", 22);
-			pImage->content = (int*) malloc(pImage->biSizeImage);
+			pImage->content = (char*) malloc(pImage->biSizeImage);
+			pImage->content[0] = currentByte;
 
-			for (unsigned int i = 0; i < pImage->biSizeImage; i++) {
+			for (unsigned int i = 1; i < pImage->biSizeImage; i++) {
 				int byte = fgetc(file);
-				printf("%d ", byte);
 				pImage->content[i] = byte;
 			}
 		}
-		*/
 	}
 }
+
 
 // int a[4] = {0x3a, 0x05, 0x00, 0x00}
 // int total = (a[0]) + (a[1] << 8) + (a[2] << 16) + (a[3] << 24)
