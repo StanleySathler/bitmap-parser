c1297a24875e38f81880ef6b879563e9372b7bc3
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-04-16 23:54:39 -0300

    chore: replace spaces by tabs in "parser.h"

--- src/parser.h
+++ src/parser.h
@@ -1,7 +1,7 @@
 #ifndef __PARSER_H__
 #define __PARSER_H__
-  #include "bitmap.h"
+	#include "bitmap.h"
 
-  void print_content(BitmapImage *pImage);
-  void print_footer(BitmapImage *pImage);
+	void print_content(BitmapImage *pImage);
+	void print_footer(BitmapImage *pImage);
 #endif /* __PARSER_H__ */
