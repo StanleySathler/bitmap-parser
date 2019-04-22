bc4c2902512ad95a2d31e69ce98d93ba398af2a8
Stanley Sathler <stanleysathlerpinto@gmail.com>
2019-04-22 18:09:28 -0300

    refac(parser): remove comments and change int types

--- src/parser.c
+++ src/parser.c
@@ -34,15 +34,15 @@ void print_content(BitmapImage *image)
 	unsigned int bytes_per_row = (bytes_per_pixel * image->biWidth);
 	unsigned int curr_index = 0;
 
-	for (int line = 0; line < image->biHeight; line++) {
-		for (int column = 0; column <= image->biWidth; column++) {
+	for (unsigned int line = 0; line < image->biHeight; line++) {
+		for (unsigned int column = 0; column <= image->biWidth; column++) {
 			if (column == image->biWidth) {
 				curr_index += bytes_per_pixel;
 				continue;
 			}
 
 			putchar('(');
-			for (int column_item_index = (bytes_per_pixel - 1); column_item_index >= 0; column_item_index--) {
+			for (int short column_item_index = (bytes_per_pixel - 1); column_item_index >= 0; column_item_index--) {
 				printf("%02X", image->content[curr_index + column_item_index]);
 
 				if (column_item_index > 0)
@@ -55,30 +55,6 @@ void print_content(BitmapImage *image)
 
 		putchar('\n');
 	}
-	/*
-	for (
-		unsigned int cursor = 0, line = 1;
-		cursor < image->biSizeImage;
-		cursor += bytes_per_pixel
-	) {
-		if (cursor == (bytes_per_row * line) + ((line - 1) * bytes_per_pixel)) {
-			line++;
-			putchar('\n');
-			continue;
-		}
-
-		putchar('(');
-
-		for (short int j = (bytes_per_pixel - 1); j >= 0; j--) {
-			if (j < (bytes_per_pixel - 1))
-				putchar(' ');
-
-			printf("%02X", image->content[cursor + j]);
-		}
-
-		putchar(')');
-	}
-	*/
 
 	putchar('\n');
 }
